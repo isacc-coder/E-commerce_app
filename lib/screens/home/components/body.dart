9ca591ec42e2900b74abc80de9c8e152fdb542e2
isacc-coder <isaccfirew12@gmail.com>
2022-05-21 12:23:49 -0400

    first commit

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:online_shop_app/models/Product.dart';
+import 'package:online_shop_app/screens/home/components/categorries.dart';
+import 'package:online_shop_app/screens/home/components/item_card.dart';
+import 'package:online_shop_app/screens/details/details_screen.dart';
+
+import '../../../constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPaddin,
+                  crossAxisSpacing: kDefaultPaddin,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) => ItemCard(
+                      product: products[index],
+                      press: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailScreen(
+                              product: products[index],
+                            ),
+                          )),
+                    )),
+          ),
+        ),
+      ],
+    );
+  }
+}
